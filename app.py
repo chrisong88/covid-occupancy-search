11b367e5b5ead0e6b7386ff6daae4c2205a543d5
Chris <christopherong@college.harvard.edu>
2021-03-10 00:45:09 -0800

    Changed back-end API structure to GET request. Now returns all results as a json object according to zipcode.

--- app.py
+++ app.py
@@ -11,11 +11,20 @@ CORS(app)
 def say_hello_world():
     return {'result': "Hello World"}
 
-@app.route('/get_occupancy/<string:zip>')
-def get_occupancy(zip):
-    # hospital_name = request.args.get('')
-    # return(json.dumps(20))
+@app.route('/get_occupancy', methods=["GET"])
+def get_occupancy():
+    zipcode = request.args.get('zipcode')
     with sql.connect("database.db") as con:
         cur = con.cursor()
-        cur.execute(f" SELECT open_adult_icu_beds FROM occupancy WHERE zip='{zip}'")
-    return(json.dumps(cur.fetchone()))
+        cur.execute(f" SELECT hospital, open_adult_icu_beds FROM occupancy WHERE zip='{zipcode}'")
+    return(json.dumps(cur.fetchall()))
+
+
+
+# def get_occupancy(zip):
+#     # hospital_name = request.args.get('')
+#     # return(json.dumps(20))
+#     with sql.connect("database.db") as con:
+#         cur = con.cursor()
+#         cur.execute(f" SELECT open_adult_icu_beds FROM occupancy WHERE zip='{zip}'")
+#     return(json.dumps(cur.fetchone()))
